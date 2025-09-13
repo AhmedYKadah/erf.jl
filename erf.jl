85de487d8e06e9fe19ed2b6e2feb7fbe0f00ccd8
AhmedsElysium <ahmadyassermo@gmail.com>
2025-09-14 02:06:17 +0300

    obtained polynomials for erf(float32)

--- erf.jl
+++ erf.jl
@@ -16,9 +16,8 @@ function erf(x::Float64)
     ix::UInt32=reinterpret(UInt64,x)>>32
     # # top 32, without sign bit 
     ia::UInt32=ix & 0x7fffffff
-    # # sign
-    # sign::UInt32=ix>>31
 
+    # # sign
     sign::Bool=x<0
 
 
@@ -152,6 +151,96 @@ function erf(x::Float64)
 end
 
 function erfc(x::Float64)
-    return 1-erf(x)
+    ## FOR TESTING
+
+p=(0x1.4bd7f18f6b4d1p-1 ,0x1.50ab5e318b039p0 ,-0x1.fc2359e57aad5p2 ,0x1.062016a67195p4 ,-0x1.660a63ba614fdp4 ,0x1.6d80e61ae504cp4 ,-0x1.1d3ffe3c75e0dp4 ,0x1.55b5a6e7c365ap3 ,-0x1.3b2721d8513bp2 ,0x1.c09ce216a88b1p0 ,-0x1.ec30d55e394bfp-2 ,0x1.9d18a13ca0ccfp-4 ,-0x1.04d43b0234125p-6 ,0x1.e06f4c3485231p-10 ,-0x1.31118ad932e84p-13 ,0x1.de4735a10bc97p-18 ,-0x1.5d40c40ce6786p-23)
+# PF=(-0x1.20dee214f8fc8p0 ,0x1.81d0e2a7e36c1p-2 ,-0x1.f33f2ec9ea38p-4 ,0x1.b1d36987ac467p-4 ,-0x1.f416301acb3c6p-2 ,0x1.18be58b89eb61p1 ,-0x1.e98d39bee4998p2 ,0x1.4f4f6f7d260f8p4 ,-0x1.6d300ebe0b722p5 ,0x1.3e036af70e6b7p6 ,-0x1.badba627b1daap6 ,0x1.ea4ef37982cb8p6 ,-0x1.aa4b57389f11fp6 ,0x1.1cf4adc0dd2e4p6 ,-0x1.1ad8f0c84b8b8p5 ,0x1.88d9a799b118ap3 ,-0x1.54d7c6059f6bdp1 , 0x1.162dff49c6e34p-2)
+#    r=evalpoly(x^2,PF)
+#    return fma(r,x,1)
+# return evalpoly(x,PF)
+return evalpoly(x,p)
+
+    ## FOR TESTING
+    
+    # # sign
+    sign::Bool=x<0
+
+
+    # # top 32 bits 
+    ix::UInt32=reinterpret(UInt64,x)>>32
+    # # top 32, without sign bit 
+    ia::UInt32=ix & 0x7fffffff
+
+    if(ia < 0x3fe00000)
+        # This branch has lower accuracy but sufficient since 
+        # all values are in the range [1,2]
+        # Highest error measured is 0.863 ULPs 
+        return 1-@inline erf(x)
+    end
+
+
+
+    # this interval needs to be editted to be a single interval
+    if (ia < 0x3feb0000)
+    #  a = |x| < 0.84375.  
+
+        x2 = x * x
+
+        if (ia < 0x3fe00000)
+        ## a < 0.5  - Use polynomial approximation.  
+
+            # Minimax approximation of erf of the form x*P(x^2) approximately on the interval [0;0.5]
+            PA=(0.12837916709551256, -0.3761263890318287, 0.11283791670896592, -0.026866170630075903, 0.005223977428649761, -0.0008548312229989974, 0.00012054654502151287, -1.4906315067498891e-5, 1.6126444349070824e-6, -1.3074259056679966e-7)
+        
+            r= fma(x,evalpoly(x2,PA),x) ## This fma is crucial for accuracy.  
+
+            return r
+        else
+        ## 0.5 <= a < 0.84375 - Use rational approximation.  
+
+            # Rational approximation on [0x1p-28, 0.84375] 
+            NA=(0x1.06eba8214db68p-3, -0x1.4cd7d691cb913p-2, -0x1.d2a51dbd7194fp-6,-0x1.7a291236668e4p-8, -0x1.8ead6120016acp-16)
+            DA=(1,0x1.97779cddadc09p-2, 0x1.0a54c5536cebap-4, 0x1.4d022c4d36b0fp-8,0x1.15dc9221c1a1p-13, -0x1.09c4342a2612p-18)
+
+            P=evalpoly(x2,NA)
+            Q=evalpoly(x2,DA)
+
+            return fma(P / Q, x, x)
+        end
+
+    # this interval needs modification or complete revision
+    elseif (ia < 0x3ff40000)
+    ## 0.84375 <= |x| < 1.25.  
+
+    elseif (ia < 0x40000000)
+    ## 1.25 <= |x| < 2.0.  
+
+    elseif (ia < 0x400a0000)
+    ## 2 <= |x| < 3.25.  
+
+    elseif (ia < 0x40100000)
+    ## 3.25 <= |x| < 4.0.  
+
+    elseif (ia < 0x4017a000)
+    ## 4 <= |x| < 5.90625.  
+
+    else
+        return 0
+    end
+
+
+
+    
+    
+    
+    
+    
+    
+    # return 1-erf(x)
 end
 
+# For testing
+# using BenchmarkTools
+# @benchmark erf(data) setup=(data=3.0)
+
+# print(erf(0.3)-0.3286267779816054)
